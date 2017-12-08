97ec9a1b986b13a5828bd10da1ec9dd3b5d8a0ad
Vaibhav Patel <vp932712@gmail.com>
2017-12-08 18:54:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 
-def speak_to_grandma(word)
-  uppercase = word.upcase
-  if word == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN"
-  elsif word == uppercase
-    "NO, NOT SINCE 1938!"
-  else
-    "HUH?! SPEAK UP, SONNY!"
-
-end
+# def speak_to_grandma(word)
+#   uppercase = word.upcase
+#   if word == "I LOVE YOU GRANDMA!"
+#     "I LOVE YOU TOO PUMPKIN"
+#   elsif word == uppercase
+#     "NO, NOT SINCE 1938!"
+#   else
+#     "HUH?! SPEAK UP, SONNY!"
+#
+# end
