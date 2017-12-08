185cf93b0a5275148091f3dbf41ad776f1d8e5c7
Vaibhav Patel <vp932712@gmail.com>
2017-12-08 18:50:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(word)
   if word == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN"
-  elsif word == uppercase
+  elsif word.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
